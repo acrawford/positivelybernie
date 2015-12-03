6dc78e7fd91c211e8b148c56add2736a9913e539
Jason Pelletier <jason.pelletier@foundline.com>
2015-12-03 08:03:38 -0500

    Add basic deployment task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
+    ghPages = require('gulp-gh-pages');
 
-gulp.task('default', function() {
+gulp.task('default', function() {});
+
+gulp.task('deploy', function() {
     return gulp.src('./index.html')
         .pipe(gulp.dest('./dist/'));
+    return gulp.src('./dist/**/*')
+        .pipe(ghPages({
+            branch: 'gh-pages',
+            message: 'Deployment at [timestamp].',
+        }));
 });
